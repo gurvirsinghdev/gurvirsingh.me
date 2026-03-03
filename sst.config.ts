af42475b71687f1b50d99664aeedf3fb09b14604
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-03-03 15:37:35 -0800

    feat(project): deploy web to aws with cloudflare dns

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,4 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -7,7 +6,24 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws: { region: "ca-central-1" },
+        cloudflare: "6.13.0",
+      },
     };
   },
-  async run() {},
+  async run() {
+    const httpPortfolio = new sst.aws.Nextjs("HttpPortfolio", {
+      path: "apps/web",
+      domain: {
+        name: "gurvirsingh.me",
+        dns: sst.cloudflare.dns({
+          zone: process.env.CLOUDFLARE_ZONE,
+        }),
+        redirects: ["www.gurvirsingh.me"],
+      },
+    });
+
+    return { httpPortfolio: httpPortfolio.url };
+  },
 });
